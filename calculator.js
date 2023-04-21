878707dd31dace3380d8162437d57e29bd3bf9f7
Son Jiui <77148574+J2020n21@users.noreply.github.com>
2023-04-21 22:56:40 +0900

    Update calculating logic and display
    Add NPM package body-parser and make it use. (R6)
    Inside the app.post, calculate 2 input number (R18-21)
    Display the result.

--- calculator.js
+++ calculator.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+//bodyPaser npm package for manipulate data.
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+//parse the data that comes from an HTML form.
+//extended:true allows to post nested objects.
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html"); 
     //__dirname: grab current file path at any given time.
 });
+
+//app.post-handle the post request specified inside ""
+app.post("/",function(req,res){
+
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+
+var result = num1 + num2;
+
+res.send("The results of the calculation is: " + result);
+
+});
+
+//req.body: parsed version of the HTTP request
+    //console.log(req.body); //form of text.
+    //console.log(req.body.num1); //html name=num1
 
 app.listen(3000,function(){
     console.log("Server Started: 3000");
